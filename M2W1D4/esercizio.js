faf9a002b9cce009ea6fa7cb72febd8f51441f5b
Dandylion <a.leone1985@gmail.com>
2026-04-24 22:37:02 +0200

    let's log this console

--- M2W1D4/esercizio.js
+++ M2W1D4/esercizio.js
@@ -11,37 +11,46 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+console.log("In Javascript abbiamo questi datatype primitivi=\n\nNUMBER: usato per scrivere i numeri\nSTRING: per variabili testuali\nBOOLEAN: per risposte TRUE\FALSE\nUNDEFINED: per variabili appena create e non ancora dichiarate\nNULL: per variabili svuotate\ntra le variabili STRUTTURALI abbiamo invece gli Oggetti, Funzioni e Array")
 
 /* ESERCIZIO 2
  Descrivi cos'è un oggetto in JavaScript, con parole tue.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+console.log("Un oggetto è una entità reale descritta attraverso gruppi di coppie chiave/valore")
+
 
 /* ESERCIZIO 3
- Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const firstNumber = 12;
+const secondNumber = 20;
+let sum = firstNumber+secondNumber;
+console.log(sum)
+
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const x = 12;
 
 /* ESERCIZIO 5
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const name = "Alessandra";
+
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let sub = (4 - x)
+console.log(sub)
+
+
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -49,5 +58,10 @@ REGOLE
  Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
  NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
 */
+const name1 = 'john'
+const name2 = 'John'
+console.log(name1 !== name2)
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let transformedName2 = name2.toLowerCase()
+console.log(name1 !== transformedName2)
+
